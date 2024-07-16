9026aa064d8f4d567e0138014491f6c6ff74d41d
Maxim <maxim@morroweyewear.com>
2024-07-16 11:35:36 +0200

    Edited theme and updated the lualine configuration. [🔧  Add or update configuration files.]
    Edited the landing page logo.
    Updated lualine configuration:
    - Added a macro recording message which replaces the git diff when
      recording a macro.
    - Added a name/title to oil buffers, which shows the name of the folder
      the buffer is in.

--- lua/maxim/lazy/lualine.lua
+++ lua/maxim/lazy/lualine.lua
@@ -6,12 +6,42 @@ local function file_modified()
     end
 end
 
+local function file_or_foldername()
+    local oil = require('oil')
+    if vim.bo.filetype == 'oil' then
+        return oil.get_current_dir()
+    else
+        return vim.fn.expand("%:t")
+    end
+end
+
+local function show_maco_recording()
+    local recording_register = vim.fn.reg_recording()
+    if recording_register == "" then
+        return ""
+    else
+        return 'rec 󰑊 macro @' .. recording_register
+    end
+end
+
+local function git_blame_or_macro_recording()
+    local macro_recording = show_maco_recording()
+    local git_blame_line = vim.b.gitsigns_blame_line
+    if macro_recording ~= "" then
+        return macro_recording
+    elseif git_blame_line ~= nil then
+        return vim.b.gitsigns_blame_line
+    else
+        return ""
+    end
+end
 
 return {
     "nvim-lualine/lualine.nvim",
     requires = { "nvim-web-devicons" },
     config = function()
         local navic = require('nvim-navic')
+        local oil = require('oil')
         local mytheme = require('maxim.lualine_theme.theme')
         local colors = require('maxim.lualine_theme.colors')
 
@@ -32,7 +62,7 @@ return {
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "diagnostics" },
-                lualine_c = { "branch", "diff", "b:gitsigns_blame_line" },
+                lualine_c = { "branch", "diff", git_blame_or_macro_recording },
                 lualine_x = { "searchcount" },
                 lualine_y = { "location" },
                 lualine_z = { "progress" },
@@ -48,7 +78,7 @@ return {
             tabline = {},
             winbar = {
                 lualine_a = {},
-                lualine_b = { "filename" },
+                lualine_b = { file_or_foldername },
                 lualine_c = {
                     {
                         function()
@@ -83,6 +113,7 @@ return {
         -- create an autocommand group to handle height
         vim.api.nvim_create_augroup("CmdHeightAdjust", { clear = true })
         local autocmd = vim.api.nvim_create_autocmd
+        -- autocommands to adjust cmdheight when entering and leaving command mode
         autocmd("CmdLineEnter", {
             group = "CmdHeightAdjust",
             callback = function()
@@ -93,6 +124,35 @@ return {
             group = "CmdHeightAdjust",
             callback = function()
                 set_cmdheight(0)
+            end,
+        })
+
+        -- create an autocommand group to refresh the lualine
+        vim.api.nvim_create_augroup("LualineRefresh", { clear = true })
+        autocmd("RecordingEnter", {
+            group = "LualineRefresh",
+            callback = function()
+                require('lualine').refresh({
+                    place = { "statusline" }
+                })
+            end,
+        })
+        autocmd("RecordingLeave", {
+            group = "LualineRefresh",
+            callback = function()
+                -- Refreshing is done after 50ms because the register does not instantly clear
+                -- after the recording ends.
+                local timer = vim.loop.new_timer()
+                timer:start(
+                    50,
+                    0,
+                    vim.schedule_wrap(function()
+                        require('lualine').refresh({
+                            place = { "statusline" }
+                        })
+                        timer:close()
+                    end)
+                )
             end,
         })
     end
