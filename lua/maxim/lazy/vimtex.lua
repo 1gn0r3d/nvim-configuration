f3252d80aa318811ce2f5bb0a28d269fb20a8fef
Maxim <vanrusselt.maxim@gmail.com>
2024-08-14 09:34:07 +0200

    Added vimtex.nvim [✨  Introduce new features.]
    - Installed vimtex.nvim for workign with latex files.

---
+++ lua/maxim/lazy/vimtex.lua
@@ -0,0 +1,28 @@
+return {
+    {
+        "lervag/vimtex",
+        lazy = false,
+        ft = { "tex", "cls" },
+        init = function()
+            vim.g.vimtex_view_general_viewer = 'SumatraPDF'
+            vim.g.vimtex_view_general_options = [[--reuse-instance --forward-search @tex @line @pdf]]
+            vim.g.vimtex_view_general_options_latexmk = [[--reuse-instance]]
+            vim.g.vimtex_quickfix_ignore_filters = {
+                'Underfull \\\\hbox',
+                'Overfull \\\\hbox',
+                'LaTeX Warning: .\\+ float specifier changed to',
+                'LaTeX hooks Warning',
+                'Package siunitx Warning: Detected the "physics" package:',
+                'Package hyperref Warning: Token not allowed in a PDF string',
+                'LaTeX Warning: Command \\\\underbar  has changed',
+                'LaTeX Warning: Command \\\\underline  has changed',
+                'Package biblatex Warning: Split bibliography detected',
+                'LaTeX Warning: There were undefined references.',
+            }
+        end
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        event = "VeryLazy",
+    },
+}
