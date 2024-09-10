2027c48598151565ad99b6f2f4765cb07433daf4
Maxim <maxim.vanrusselt@imec.be>
2024-09-10 15:02:29 +0200

    Telescope & Harpoon working. [🔧  Add or update configuration files.]

--- lua/maxim/lazy/telescope.lua
+++ lua/maxim/lazy/telescope.lua
@@ -1,7 +1,7 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-        "plenary",
+        "nvim-lua/plenary.nvim",
         {
             "nvim-telescope/telescope-fzf-native.nvim",
             build = "make",
