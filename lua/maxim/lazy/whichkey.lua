5075d108b72be718b703fadbf97ff428daf69f75
Maxim <maxim.vanrusselt@imec.be>
2024-09-10 14:09:02 +0200

    Treesitter working. [🔧  Add or update configuration files.]
    Got treesitter working by disabling default interpreters.
    Now need to set up harpoon and telescope.

--- lua/maxim/lazy/whichkey.lua
+++ lua/maxim/lazy/whichkey.lua
@@ -5,24 +5,26 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
     end,
-    opts = {},
-    config = function()
-        require('which-key').register({
-            ["<leader>c"] = { name = "Copilot ..." },
-            ["<leader>cc"] = { name = "CopilotChat ..." },
-            ["<leader>g"] = { name = "Git related operations [Gitmoji, NeoGit, Git fugitive]" },
-            ["<leader>gt"] = { name = "Toggle ..." },
-            ["<leader>h"] = { name = "Remove search highlights." },
-            ["<leader>m"] = { name = "~~ MAKE IT RAIN! ~~" },
-            ["<leader>p"] = { name = "Project ..." },
-            ["<leader>pW"] = { name = "Word ..." },
-            ["<leader>pw"] = { name = "Word (with brackets) ..." },
-            ["<leader>t"] = { name = "Trouble ..." },
-            ["<leader>v"] = { name = "LSP" },
-            ["<leader>vc"] = { name = "Code actions." },
-            ["<leader>vr"] = { name = "References & rename" },
-            ["<leader>vw"] = { name = "Search workspace symbols" },
-            ["<leader>z"] = { name = "Zen mode" },
-        })
-    end
+    opts = {
+        notify = false,
+    },
+    -- config = function()
+    --     require('which-key').register({
+    --         { "<leader>c",  group = "Copilot ..." },
+    --         { "<leader>cc", group = "CopilotChat ..." },
+    --         { "<leader>g",  group = "Git related operations [Gitmoji, NeoGit, Git fugitive]" },
+    --         { "<leader>gt", group = "Toggle ..." },
+    --         { "<leader>h",  group = "Remove search highlights." },
+    --         { "<leader>m",  group = "~~ MAKE IT RAIN! ~~" },
+    --         { "<leader>p",  group = "Project ..." },
+    --         { "<leader>pW", group = "Word ..." },
+    --         { "<leader>pw", group = "Word (with brackets) ..." },
+    --         { "<leader>t",  group = "Trouble ..." },
+    --         { "<leader>v",  group = "LSP" },
+    --         { "<leader>vc", group = "Code actions." },
+    --         { "<leader>vr", group = "References & rename" },
+    --         { "<leader>vw", group = "Search workspace symbols" },
+    --         { "<leader>z",  group = "Zen mode" },
+    --     })
+    -- end
 }
